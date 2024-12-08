e7009f4232022badc8acf8cdef9397d93a3e89b6
FelixHartramf <Felix>
2024-12-08 14:19:18 +0100

    removale readonly vars

--- src/day8.rs
+++ src/day8.rs
@@ -33,19 +33,14 @@ pub fn part1(input: &str) -> usize {
                     continue;
                 }
 
-                let x1 = b1.0;
-                let x2 = b2.0;
-                let y1 = b1.1;
-                let y2 = b2.1;
+                let dx =  b2.0 - b1.0;
+                let dy = b2.1 - b1.1;
 
-                let dx = x2 - x1;
-                let dy = y2 - y1;
+                let x3 = b1.0 - dx;
+                let y3 = b1.1 - dy;
 
-                let x3 = x1 - dx;
-                let y3 = y1 - dy;
-
-                let x4 = x2 + dx;
-                let y4 = y2 + dy;
+                let x4 =  b2.0 + dx;
+                let y4 = b2.1 + dy;
 
                 if x3 >= 0 && x3 < x_max && y3 >= 0 && y3 < y_max {
                     locations.push((x3, y3));
@@ -95,31 +90,26 @@ pub fn part2(input: &str) -> usize {
                     continue;
                 }
 
-                let x1 = b1.0;
-                let x2 = b2.0;
-                let y1 = b1.1;
-                let y2 = b2.1;
-
-                let dx = x2 - x1;
-                let dy = y2 - y1;
+                let dx = b2.0 - b1.0;
+                let dy = b2.1 - b1.1;
 
                 let mut i = 0;
-                while x1 - (i * dx) >= 0
-                    && x1 - (i * dx) < x_max
-                    && y1 - (i * dy) >= 0
-                    && y1 - (i * dy) < y_max
+                while b1.0 - (i * dx) >= 0
+                    && b1.0 - (i * dx) < x_max
+                    && b1.1 - (i * dy) >= 0
+                    && b1.1 - (i * dy) < y_max
                 {
-                    locations.push((x1 - (i * dx), y1 - (i * dy)));
+                    locations.push((b1.0 - (i * dx), b1.1 - (i * dy)));
                     i += 1;
                 }
 
                 i = 0;
-                while x2 + (i * dx) >= 0
-                    && x2 + (i * dx) < x_max
-                    && y2 + (i * dy) >= 0
-                    && y2 + (i * dy) < y_max
+                while b2.0 + (i * dx) >= 0
+                    && b2.0 + (i * dx) < x_max
+                    && b2.1 + (i * dy) >= 0
+                    && b2.1 + (i * dy) < y_max
                 {
-                    locations.push((x2 + (i * dx), y2 + (i * dy)));
+                    locations.push((b2.0 + (i * dx), b2.1 + (i * dy)));
                     i += 1;
                 }
             }
