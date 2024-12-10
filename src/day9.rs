af4c0eae98f7c5537c4404caa17ae64c534921ef
FelixHartramf <Felix>
2024-12-10 15:53:38 +0100

    d9p2 first slow solution

--- src/day9.rs
+++ src/day9.rs
@@ -6,7 +6,7 @@ pub fn part1(input: &str) -> u64 {
     let mut lp = 0;
     let mut l_appearnce = v[0] as u8 - 48;
     let mut rp = v.len() - 2;
-    let mut r_appearnce = v[rp] as u8  - 48;
+    let mut r_appearnce = v[rp] as u8 - 48;
 
     let mut sum: u64 = 0;
     let mut i = 0;
@@ -25,7 +25,7 @@ pub fn part1(input: &str) -> u64 {
 
                 while r_appearnce == 0 {
                     rp -= 2;
-                    r_appearnce = v[rp] as u8  - 48;
+                    r_appearnce = v[rp] as u8 - 48;
                 }
                 r_appearnce -= 1;
                 l_appearnce -= 1;
@@ -47,7 +47,54 @@ pub fn part1(input: &str) -> u64 {
 
 #[aoc(day9, part2)]
 pub fn part2(input: &str) -> u64 {
-    0
+    let v = input.as_bytes();
+
+    let mut files: Vec<(u64, u8, bool)> = vec![]; // file_id, size, file?
+
+    for (value, i) in v[..v.len() - 1].iter().zip(0..) {
+        files.push((i / 2, *value as u8 - 48, i % 2 == 0));
+    }
+
+    'bigloop: for i in (0..files.len()).rev() {
+        if !files[i].2 {
+            continue;
+        }
+
+        for k in 0..i {
+            if files[k].2 {
+                continue;
+            }
+
+            if files[k].1 < files[i].1 {
+                continue;
+            }
+            if files[k].1 == files[i].1 {
+                files.swap(i, k);
+            }
+
+            if files[k].1 > files[i].1 {
+                files[k].1 -= files[i].1;
+                files.insert(k, files[i]);
+                files[i + 1].2 = false;
+            }
+            continue 'bigloop;
+        }
+    }
+
+    let mut sum = 0;
+    let mut i = 0;
+    for file in files.iter_mut() {
+        if file.2 {
+            while file.1 > 0 {
+                sum += file.0 * i;
+                file.1 -= 1;
+                i += 1;
+            }
+        } else {
+            i += file.1 as u64;
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -65,9 +112,27 @@ mod tests {
             1928
         );
 
-        assert_eq!(part1(&fs::read_to_string("input/2024/day9.txt").expect("")), 6330095022244);
+        assert_eq!(
+            part1(&fs::read_to_string("input/2024/day9.txt").expect("")),
+            6330095022244
+        );
     }
 
     #[test]
-    fn part2_test() {}
+    fn part2_test() {
+        assert_eq!(
+            part2(
+                "2333133121414131402
+"
+            ),
+            2858
+        );
+
+        assert!(part2(&fs::read_to_string("input/2024/day9.txt").expect("")) > 6358495490494);
+        assert!(part2(&fs::read_to_string("input/2024/day9.txt").expect("")) < 63584954904941);
+        assert_eq!(part2(
+            &fs::read_to_string("input/2024/day9.txt").expect("")
+       ),
+6359491814941);
+    }
 }
