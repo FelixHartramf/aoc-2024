47443b105c68de26e5b9983dc4201682e5ca4c8b
FelixHartramf <Felix>
2024-12-11 18:23:13 +0100

    d11 speed improvements

--- src/day11.rs
+++ src/day11.rs
@@ -4,50 +4,11 @@ use aoc_runner_derive::aoc;
 
 #[aoc(day11, part1)]
 pub fn part1(input: &str) -> u64 {
-    let nums = input
+    let mut hm  = HashMap::with_capacity(3100);
+    input
         .split_ascii_whitespace()
-        .map(|s| s.parse::<u64>().unwrap())
-        .collect::<Vec<u64>>();
-
-    let mut sum = 0;
-    let mut hm  = HashMap::new();
-    for num in nums {
-        sum += mul_num(num, 25, &mut hm);
-    }
-
-    sum
-}
-
-pub fn process_num(num: u64) -> Vec<u64> {
-    if num == 0 {
-        return vec![1];
-    }
-
-    let dec_len = (num as f64).log10() as u32 + 1;
-    if dec_len % 2 == 0 {
-        return vec![
-            num / (10_u64.pow(dec_len / 2)),
-            num % (10_u64.pow(dec_len / 2)),
-        ];
-    }
-
-    vec![num * 2024]
-}
-
-#[aoc(day11, part2)]
-pub fn part2(input: &str) -> u64 {
-    let nums = input
-        .split_ascii_whitespace()
-        .map(|s| s.parse::<u64>().unwrap())
-        .collect::<Vec<u64>>();
-
-    let mut sum = 0;
-    let mut hm  = HashMap::new();
-    for num in nums {
-        sum += mul_num(num, 75, &mut hm);
-    }
-
-    sum
+        .map(|s| mul_num(s.parse::<u64>().unwrap(), 25, &mut hm))
+        .sum()
 }
 
 pub fn mul_num(num: u64, steps_to_take: u64, hm: &mut HashMap<(u64, u64), u64>) -> u64{
@@ -57,30 +18,56 @@ pub fn mul_num(num: u64, steps_to_take: u64, hm: &mut HashMap<(u64, u64), u64>) 
     if hm.contains_key(&(num, steps_to_take)){
         return *hm.get(&(num, steps_to_take)).unwrap();
     }
-    let mut sum = 0;
-    for n in process_num(num){
-       let v = mul_num(n, steps_to_take-1, hm); 
-        hm.insert((n, steps_to_take-1), v);
-       sum += v;
-    }
+    let sum = match process_num(num) {
+        Mul::One(p) => mul_num(p, steps_to_take-1, hm),
+        Mul::Two(p1, p2) => mul_num(p1, steps_to_take-1, hm) + mul_num(p2, steps_to_take-1, hm),
+    };
     
+    hm.insert((num, steps_to_take), sum);
 
     sum
 }
 
+#[derive(Debug)]
+pub enum Mul {
+    One(u64),
+    Two(u64,u64)
+}
+pub fn process_num(num: u64) -> Mul{
+    if num == 0 {
+        return Mul::One(1);
+    }
+
+    let dec_len = (num as f64).log10() as u32 + 1;
+    if dec_len % 2 == 0 {
+        return Mul::Two(
+            num / (10_u64.pow(dec_len / 2)),
+            num % (10_u64.pow(dec_len / 2)),
+    );
+    }
+
+    Mul::One(num * 2024)
+}
+
+#[aoc(day11, part2)]
+pub fn part2(input: &str) -> u64 {
+    let mut hm  = HashMap::with_capacity(125000);
+    input
+        .split_ascii_whitespace()
+        .map(|s| mul_num(s.parse::<u64>().unwrap(), 75, &mut hm))
+        .sum()
+}
+
+
+
 #[cfg(test)]
 mod tests {
 
-    use super::{part1, part2, process_num};
+    use super::{part1, part2};
     use std::fs;
 
     #[test]
     fn part1_test() {
-        assert_eq!(process_num(125), vec![253000]);
-        assert_eq!(process_num(17), vec![1, 7]);
-        assert_eq!(process_num(253000), vec![253, 0]);
-        assert_eq!(part1("125 17"), 55312);
-
         assert_eq!(
             part1(&fs::read_to_string("input/2024/day11.txt").expect("")),
             202019
