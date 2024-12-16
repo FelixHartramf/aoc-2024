ee26b1aa868024b8986f10403ae1f18af92a6e68
FelixHartramf <Felix>
2024-12-16 20:51:48 +0100

    d16: first realy slow solution

---
+++ src/day16.rs
@@ -0,0 +1,274 @@
+use aoc_runner_derive::aoc;
+
+use pathfinding::prelude::astar;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Reindeer {
+    y: usize,
+    x: usize,
+    direction: Direction,
+}
+
+impl Reindeer {
+    fn new(y: usize, x: usize, dir: Direction) -> Self {
+        Self {
+            y: y,
+            x: x,
+            direction: dir,
+        }
+    }
+
+    fn heuristic(&self, goal: (usize, usize)) -> usize {
+        self.x.abs_diff(goal.0) + self.y.abs_diff(goal.1)
+    }
+
+    fn finished(&self, finish: (usize, usize)) -> bool {
+        self.y == finish.0 && self.x == finish.1
+    }
+
+    fn successors(&self, grid: &Vec<Vec<char>>) -> Vec<(Reindeer, usize)> {
+        
+
+
+        let mut succ = match self.direction {
+            Direction::East | Direction::West => {
+                vec![
+                    (Reindeer::new(self.y, self.x, Direction::South), 1000),
+                    (Reindeer::new(self.y, self.x, Direction::North), 1000),
+                ]
+            }
+
+            Direction::North | Direction::South => {
+                vec![
+                    (Reindeer::new(self.y, self.x, Direction::East), 1000),
+                    (Reindeer::new(self.y, self.x, Direction::West), 1000),
+                ]
+            }
+        };
+
+        if self.direction == Direction::East && grid[self.y][self.x + 1] != '#' {
+            succ.push((Reindeer::new(self.y, self.x + 1, Direction::East), 1));
+        }
+
+        if self.direction == Direction::West && grid[self.y][self.x - 1] != '#' {
+            succ.push((Reindeer::new(self.y, self.x - 1, Direction::West), 1));
+        }
+
+        if self.direction == Direction::South && grid[self.y + 1][self.x] != '#' {
+            succ.push((Reindeer::new(self.y + 1, self.x, Direction::South), 1));
+        }
+
+        if self.direction == Direction::North && grid[self.y - 1][self.x] != '#' {
+            succ.push((Reindeer::new(self.y - 1, self.x, Direction::North), 1));
+        }
+
+        succ
+    }
+}
+
+#[aoc(day16, part1)]
+pub fn part1(input: &str) -> usize {
+    let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    let mut reindeer = (0, 0);
+    let mut end = (0, 0);
+    
+    // ToDo: integrate in grid parsing
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            if grid[y][x] == 'S' {
+                reindeer = (y, x);
+            }
+
+            if grid[y][x] == 'E' {
+                end = (y, x);
+            }
+        }
+    }
+
+    astar(
+        &Reindeer::new(reindeer.0, reindeer.1, Direction::East),
+        |r| r.successors(&grid),
+        |r| r.heuristic((end.0, end.1)),
+        |r| r.finished((end.0, end.1)),
+    ).unwrap().1
+}
+
+#[aoc(day16, part2)]
+pub fn part2(input: &str) -> usize {
+    let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    let mut reindeer = (0, 0);
+    let mut end = (0, 0);
+    
+    // ToDo: integrate in grid parsing
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            if grid[y][x] == 'S' {
+                reindeer = (y, x);
+            }
+
+            if grid[y][x] == 'E' {
+                end = (y, x);
+            }
+        }
+    }
+
+    let best = astar(
+        &Reindeer::new(reindeer.0, reindeer.1, Direction::East),
+        |r| r.successors(&grid),
+        |r| r.heuristic((end.0, end.1)),
+        |r| r.finished((end.0, end.1)),
+    ).unwrap().1;
+
+    let mut sum = 0;
+
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            if grid[y][x] == '#' {
+                continue;
+            }
+
+            let to_solution_meow = astar(
+                &Reindeer::new(reindeer.0, reindeer.1, Direction::East),
+                |r| r.successors(&grid),
+                |r| r.heuristic((y, x)),
+                |r| r.finished((y, x)),
+            );
+            if to_solution_meow == None {
+                continue;
+            }
+
+            let to_solution = to_solution_meow.unwrap();
+            let to = to_solution.1;
+
+            if to > best+1 {
+                continue;
+            }
+
+            let from = astar(
+                dbg!(to_solution.0.last().unwrap()),
+                |r| r.successors(&grid),
+                |r| r.heuristic((end.0, end.1)),
+                |r| r.finished((end.0, end.1)),
+            ).unwrap().1;
+
+            if to+from == best {
+                sum += 1;
+            }
+        }
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::{part1, part2};
+    use std::fs;
+
+    #[test]
+    fn part1_test() {
+        assert_eq!(
+            part1(
+                "###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############"
+            ),
+            7036
+        );
+
+        assert_eq!(
+            part1(
+                "#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################"
+            ),
+            11048
+        );
+        assert_eq!(part1(&fs::read_to_string("input/2024/day16.txt").expect("")), 107468);
+    }
+
+    #[test]
+    fn part2_test() {
+        assert_eq!(
+            part2(
+                "###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############"
+            ),
+            45
+        );
+
+        assert_eq!(
+            part2(
+                "#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################"
+            ),
+            64
+        );
+        assert_eq!(part2(&fs::read_to_string("input/2024/day16.txt").expect("")), 533);
+    }
+}
